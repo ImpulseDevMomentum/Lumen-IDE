9f00fb885d22c5838b06258177f2e1586c0db297
ImpulseDevMomentum <97831192+ImpulseDevMomentum@users.noreply.github.com>
2024-12-31 19:35:10 +0100

    fix open themes & highlights not opening bug

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electron', {
     ipcRenderer: {
         invoke: (channel, data) => {
-            const validChannels = ['open-file', 'save-file', 'run-code', 'stop-code', 'console-input', 'save-settings', 'load-settings', 'load-themes', 'load-highlights', 'open-docs']
+            const validChannels = ['open-file', 'save-file', 'run-code', 'stop-code', 'console-input', 'save-settings', 'load-settings', 'load-themes', 'load-highlights', 'open-docs', 'open-themes-folder', 'open-highlights-folder']
             if (validChannels.includes(channel)) {
                 return ipcRenderer.invoke(channel, data)
             }
